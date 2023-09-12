1957b28a11cd4db1870f2903c1821fa03af34d1b
Tony <fc0302@naver.com>
2023-09-12 14:27:20 +0700

    Initial Commit

---
+++ Tunexa/TunexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunexaApp.swift
+//  Tunexa
+//
+//  Created by Tony on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct TunexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
