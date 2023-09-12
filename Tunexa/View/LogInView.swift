a8ed6859ebc537b1525d299dc467c7a7b26e4e25
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-09-12 15:15:46 +0700

    update view folder

---
+++ Tunexa/View/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  Tunexa
+//
+//  Created by Nguyễn Anh Duy on 12/09/2023.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
---
+++ Tunexa/View/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  Tunexa
+//
+//  Created by Nguyễn Anh Duy on 12/09/2023.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
