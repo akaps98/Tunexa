2a4394058d3a9d57dd81b61f3130817f11f34c1d
vietngg21 <viet.ngg21@gmail.com>
2023-09-13 18:22:00 +0700

    added slider for song progress

--- Tunexa/View/SongCardView.swift
+++ Tunexa/View/SongCardView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 
 struct SongCardView: View {
+    @State private var currentSongPosition: Double = 0.0
+    let totalSongDuration: Double = 210.0  // for a 4-minute song, for instance
+    var formattedTotalDuration: String {
+        let minutes = Int(totalSongDuration) / 60
+        let seconds = Int(totalSongDuration) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    var currentTime: String {
+        let minutes = Int(currentSongPosition) / 60
+        let seconds = Int(currentSongPosition) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
     
     var body: some View {
         ZStack {
@@ -27,6 +39,7 @@ struct SongCardView: View {
                     Spacer()
                     
                     Text("Now Playing")
+                        .font(.custom("Nunito-Bold", size: 20))
                     
                     Spacer()
                     
@@ -63,30 +76,47 @@ struct SongCardView: View {
                 
                 Spacer()
                 
-                // MARK: AUDIO SLIDER
+                // MARK: SONG PROGRESS SLIDER
                 HStack {
-                    
+                    // Text representing current time in the song
+                    Text(currentTime)
+                        .font(.caption)
+                        .padding(.leading)
+
+                    // The progress slider itself
+                    Slider(value: $currentSongPosition, in: 0.0...totalSongDuration, onEditingChanged: { isEditing in
+                        if !isEditing {
+                            // Handle the change in song position here when the user finishes dragging the slider.
+                            // This might involve seeking the audio to the correct position.
+                        }
+                    })
+                    .padding(.horizontal)
+
+                    // Text representing total duration of the song
+                    Text(formattedTotalDuration)
+                        .font(.caption)
+                        .padding(.trailing)
                 }
-                
+
                 Spacer()
         
                 // MARK: AUDIO CONTROLS
                 HStack {
                     Spacer()
                     
-                    HStack {
+                    HStack(spacing: 30) {
                         Button {
                             print("Repeat")
                         } label: {
                             Image(systemName: "repeat")
-                                .font(.system(size: 25))
+                                .font(.system(size: 22))
                                 .foregroundColor(Color("text-color"))
                         }
                         Button {
                             print("Backward")
                         } label: {
                             Image(systemName: "backward.end.fill")
-                                .font(.system(size: 25))
+                                .font(.system(size: 22))
                                 .foregroundColor(Color("text-color"))
                         }
                     }
@@ -99,23 +129,24 @@ struct SongCardView: View {
                         Image(systemName: "play.circle.fill")
                             .font(.system(size: 50))
                             .foregroundColor(Color("text-color"))
+                        
                     }
                     
                     Spacer()
                     
-                    HStack {
+                    HStack(spacing: 30) {
                         Button {
                             print("Forward")
                         } label: {
                             Image(systemName: "forward.end.fill")
-                                .font(.system(size: 25))
+                                .font(.system(size: 22))
                                 .foregroundColor(Color("text-color"))
                         }
                         Button {
                             print("Shuffle")
                         } label: {
                             Image(systemName: "shuffle")
-                                .font(.system(size: 25))
+                                .font(.system(size: 22))
                                 .foregroundColor(Color("text-color"))
                         }
                     }
