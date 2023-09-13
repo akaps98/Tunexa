1f998a0bc67e03576d63b191b72dc9edca828aa4
vietngg21 <viet.ngg21@gmail.com>
2023-09-13 16:02:40 +0700

    viet-workspace

---
+++ Tunexa/View/SongCardView.swift
@@ -0,0 +1,20 @@
+//
+//  SongCardView.swift
+//  Tunexa
+//
+//  Created by Viet Nguyen Hoang on 13/09/2023.
+//
+
+import SwiftUI
+
+struct SongCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SongCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongCardView()
+    }
+}
