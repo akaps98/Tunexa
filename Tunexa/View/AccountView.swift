c851e913cb807c55fa7aec9da46e1fd89ce48bf8
Tony <fc0302@naver.com>
2023-09-14 01:27:26 +0700

    make the profile pic on Account view bigger

--- Tunexa/View/AccountView.swift
+++ Tunexa/View/AccountView.swift
@@ -28,28 +28,29 @@ struct AccountView: View {
                     // MARK: - BACKGROUND COLOR
                     Color("bg-color")
                         .edgesIgnoringSafeArea(.all)
-                    VStack {
-                        // MARK: - MY ACCOUNT TEXT
-                        Text("My account")
-                            .font(.custom("Nunito-Bold", size: 37))
-                        // MARK: - PROFILE PIC
-                        pickedImage
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle().inset(by: 50))
-                            .offset(y:-70)
-                        // MARK: - LINK TO ACCOUNTEDIT VIEW
-                        NavigationLink(destination: AccountEditView(isDark: $isDark)) { Text("Edit profile...") }.offset(y:-115)
-                        Group {
-                            Image(systemName: "person.crop.circle")
+                    ScrollView {
+                        VStack {
+                            // MARK: - MY ACCOUNT TEXT
+                            Text("My account")
+                                .font(.custom("Nunito-Bold", size: 37))
+                            // MARK: - PROFILE PIC
+                            pickedImage
                                 .resizable()
-                                .frame(width: 40, height: 40)
-                            // MARK: - EMAIL
-                            Text("User: \(email)")
-                                .font(.custom("Nunito-Light", size: 30))
-                            // MARK: - INTRODUCTION
-                            GroupBox(label:
-                                    Label("\(Image(systemName: "pencil.line"))  Introduction", systemImage: "")
+                                .scaledToFit()
+                                .clipShape(Circle().inset(by: 50))
+                                .offset(y:-70)
+                            // MARK: - LINK TO ACCOUNTEDIT VIEW
+                            NavigationLink(destination: AccountEditView(isDark: $isDark)) { Text("Edit profile...") }.offset(y:-115)
+                            Group {
+                                Image(systemName: "person.crop.circle")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                // MARK: - EMAIL
+                                Text("User: \(email)")
+                                    .font(.custom("Nunito-Light", size: 30))
+                                // MARK: - INTRODUCTION
+                                GroupBox(label:
+                                            Label("\(Image(systemName: "pencil.line"))  Introduction", systemImage: "")
                                     .font(.custom("Nunito-SemiBold", size: 26))
                                 ) {
                                     ScrollView(.vertical, showsIndicators: true) {
@@ -59,7 +60,8 @@ struct AccountView: View {
                                     }
                                     .frame(height: 140)
                                 }
-                        }.offset(y:-90)
+                            }.offset(y:-90)
+                        }
                     }
                 }
             }.environment(\.colorScheme, isDark ? .dark : .light) // modify the color sheme based on the state variable
