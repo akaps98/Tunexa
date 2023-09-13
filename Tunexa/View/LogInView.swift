e5a83f216f46bdeb73765079f5a97f1e266625ea
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2023-09-13 16:54:47 +0700

    front base

--- Tunexa/View/LogInView.swift
+++ Tunexa/View/LogInView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct LogInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Log in View")
+                        .font(.largeTitle)
+                        .bold()
+                    Spacer()
+                }
+                
+                HStack {
+                    Image(systemName: "mail")
+                    TextField("Email", text: $email)
+                    Spacer()
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                )
+                
+                HStack {
+                    Image(systemName: "lock")
+                    TextField("Password", text: $password)
+                    Spacer()
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                )
+                
+                Button {
+                    print("To Sign up View")
+                } label: {
+                    Text("Don't have an account?")
+                }
+                
+                Button {
+                    print($email, $password)
+                } label: {
+                    Text("Log in")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .bold()
+                                    
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                                    
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                        )
+                        .padding(.horizontal)
+                }
+                
+            }.padding()
+        }
     }
 }
 
--- Tunexa/View/SignUpView.swift
+++ Tunexa/View/SignUpView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Sign up View")
+                        .font(.largeTitle)
+                        .bold()
+                    Spacer()
+                }
+                
+                HStack {
+                    Image(systemName: "mail")
+                    TextField("Email", text: $email)
+                    Spacer()
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                )
+                
+                HStack {
+                    Image(systemName: "lock")
+                    TextField("Password", text: $password)
+                    Spacer()
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 2)
+                )
+                
+                Button {
+                    print("to Log In View")
+                } label: {
+                    Text("Already have an account?")
+                }
+                
+                Button {
+                    print($email, $password)
+                    
+                    
+                } label: {
+                    Text("Create account")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .bold()
+                                    
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                                    
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                        )
+                        .padding(.horizontal)
+                }
+                
+            }.padding()
+        }
     }
 }
 
