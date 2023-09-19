a49bc9fd2e2f529dd5723feefb984740db99a87f
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-09-19 19:51:44 +0700

    fix json file

--- Tunexa/Helpers/ModelData.swift
+++ Tunexa/Helpers/ModelData.swift
@@ -30,7 +30,7 @@ func decodeJsonFromJsonFile<T: Codable>(jsonFileName: String) -> [T] {
     return [] as [T]
 }
 
-var artists: [Artist] = decodeJsonFromJsonFile(jsonFileName: "artists.json")
+var artists: [Artist] = decodeJsonFromJsonFile(jsonFileName: "artist.json")
 var songs: [Song] = decodeJsonFromJsonFile(jsonFileName: "songs.json")
-var categories: [Category] = decodeJsonFromJsonFile(jsonFileName: "categories.json")
+var categories: [Category] = decodeJsonFromJsonFile(jsonFileName: "category.json")
 
